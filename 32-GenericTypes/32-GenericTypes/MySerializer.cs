18f82812ecc380990323792818b967f31e277891
Kubra Bosnak <skubrab@gmail.com>
2022-05-23 01:01:31 +0300

    C# Çalışmaları
    Generic Kavramı, Generic Classes, Generic Methods, Generic Constraints

---
+++ 32-GenericTypes/32-GenericTypes/MySerializer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace _32_GenericTypes
+{
+    public class MySerializer<T>
+    {
+        public void Serialize(string path, T obj) // Dosyaya xml yazdırma
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            StreamWriter streamWriter = new StreamWriter(path);
+
+            serializer.Serialize(streamWriter, obj);
+
+            streamWriter.Close();
+            streamWriter.Dispose();
+        }
+        public string Serialize(T obj) //Ram seviyesinde xml yazdırma
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            MemoryStream memoryStream = new MemoryStream();
+            StreamWriter streamWriter = new StreamWriter(memoryStream);
+
+            serializer.Serialize(streamWriter, obj);
+
+            memoryStream.Position = 0;
+
+            StreamReader streamReader = new StreamReader(memoryStream);
+            string result = streamReader.ReadToEnd();
+
+            streamWriter.Close();
+            streamWriter.Dispose();
+
+            streamReader.Close();
+            streamReader.Close();
+
+            memoryStream.Close();
+            memoryStream.Dispose();
+
+            return result;
+        }
+
+
+        public T Deserialize(string path)
+        {
+            if (File.Exists(path))
+            {
+                StreamReader streamReader = new StreamReader(path);
+                XmlReader xmlReader = new XmlTextReader(streamReader);
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+                if (serializer.CanDeserialize(xmlReader))
+                {
+                    streamReader.BaseStream.Position = 0;
+                    object obj = serializer.Deserialize(streamReader);
+
+                    xmlReader.Close();
+                    streamReader.Close();
+
+                    return (T)obj;
+                }
+            }
+            return default(T);
+        }
+    }
+}
---
+++ 32-GenericTypes/32-GenericTypes/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+namespace _32_GenericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GenericClassNasilYazilir();
+
+            // MyGenericXMLSerializerMethod();
+
+            BasicGenericClassSample();
+        }
+
+        private static void BasicGenericClassSample()
+        {
+            bool isEqual = EqualityChecker.AreEqual<int>(5, 6);
+            // bool isEqual = EqualityChecker.AreEqual<string>("5", "6");
+
+            if (isEqual)
+            {
+                Console.WriteLine("İki değer birbirine eşit.");
+            }
+            else
+            {
+                Console.WriteLine("İki değer birbirine eşit değil.");
+            }
+        }
+
+        // Static bir class ya da method new lenmeden çağırılır. Static olmayanlar ise newlenerek çağırılır.
+
+        private static void MyGenericXMLSerializerMethod()
+        {
+            List<string> data = new List<string> { "aaa", "bbb", "ccc", "ddd" };
+
+            MySerializer<List<string>> mySerializer = new MySerializer<List<string>>();
+
+            mySerializer.Serialize("data.xml", data);
+            List<string> result = mySerializer.Deserialize("data.xml");
+
+            string xml = mySerializer.Serialize(data);
+        }
+
+        private static void GenericClassNasilYazilir()
+        {
+            Company<string, bool> company = new Company<string, bool>
+            {
+                Name = "BilgeAdam",
+                Address = "Bilkent",
+                Data = new List<string>() { "Ahmet", "Mehmet", "Murat" }
+            };
+            Company<decimal, string> company2 = new Company<decimal, string>
+            {
+                Name = "BilgeAdam2",
+                Address = "Bilkent2",
+                Data = new List<decimal>() { 123, 234, 345 }
+            };
+        }
+    }
+
+    class Company<T,K> //<T> BU şekilde class ı generic liste haline getirdi. T type ı ifade eder. T-K-L-M diye sıralanabilir.
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<T> Data { get; set; }
+
+        public void DoWork(T val)
+        {
+            throw new NotImplementedException();
+        }
+        public T DoWork(T val,int val2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EqualityChecker
+    {
+        public static bool AreEqual<T>(T v1, T v2)
+        {
+            return v1.Equals(v2);
+        }
+    }
+
+}
